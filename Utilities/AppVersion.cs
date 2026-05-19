1f23eed93e6cbf4d0c5612efd5a6adf50fa8852f
Marios Giakalaras <mgiakalaras@hotmail.com>
2026-05-19 16:34:43 +0300

    Fix mobile menu visibility

--- Utilities/AppVersion.cs
+++ Utilities/AppVersion.cs
@@ -2,10 +2,10 @@ namespace SchoolInventoryManager.Utilities;
 
 public static class AppVersion
 {
-    public const string Current = "0.1.8";
+    public const string Current = "0.1.9";
     public const string Version = Current;
 
-    public const string ReleaseName = "Mobile menu overlay fix and metadata cleanup";
+    public const string ReleaseName = "Mobile menu visibility fix";
     public const string Channel = "Development / School testing";
 
     public const string RepositoryUrl = "https://github.com/mgiakalaras/SchoolInventoryManager";
