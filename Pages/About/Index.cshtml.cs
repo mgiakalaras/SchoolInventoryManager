27b62ed39e00b000637dea24c7ec30e3a5081416
Marios Giakalaras <mgiakalaras@hotmail.com>
2026-05-26 10:05:27 +0300

    Fix About page Razor escape

--- Pages/About/Index.cshtml.cs
+++ Pages/About/Index.cshtml.cs
@@ -1,11 +1,10 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using SchoolInventoryManager.Utilities;
 
 namespace SchoolInventoryManager.Pages.About;
 
 public class IndexModel : PageModel
 {
-    public string Version => AppVersion.Current;
-    public string Channel => AppVersion.Channel;
-    public string RepositoryUrl => AppVersion.RepositoryUrl;
+    public void OnGet()
+    {
+    }
 }
--- Pages/Contact/Index.cshtml.cs
+++ Pages/Contact/Index.cshtml.cs
@@ -4,4 +4,7 @@ namespace SchoolInventoryManager.Pages.Contact;
 
 public class IndexModel : PageModel
 {
+    public void OnGet()
+    {
+    }
 }
